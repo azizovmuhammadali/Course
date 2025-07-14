129f14248516b8a569fffc1c51c3fb928f2f4675
azizovmuhammadali <alico.777777@gmail.com>
2025-07-15 01:40:22 +0500

    changed into parts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\V1\api\Admin\AdminController;
+use App\Http\Controllers\V1\api\Manager\ManagerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+    Route::prefix('admin')->group(function () {
+        require __DIR__.'/admin.php';   
+    });
+
+    Route::prefix('manager')->group(function () {
+        require __DIR__.'/manager.php';    
+    });
