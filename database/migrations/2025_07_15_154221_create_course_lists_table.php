3f2f9bc87ab8fa2581a100f71cce7724460ae3e1
azizovmuhammadali <alico.777777@gmail.com>
2025-07-15 23:22:12 +0500

    Created course list model and migration

---
+++ database/migrations/2025_07_15_154221_create_course_lists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 8, 3);
+            $table->integer('period')->default(12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_lists');
+    }
+};
