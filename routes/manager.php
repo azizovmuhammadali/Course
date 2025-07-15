1abb32c1c65778d5eb94b2cd4af2334a22074a77
azizovmuhammadali <alico.777777@gmail.com>
2025-07-15 23:22:57 +0500

    Added course routes

--- routes/manager.php
+++ routes/manager.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\V1\api\Manager\ManagerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\V1\api\Manager\ManagerController;
+use App\Http\Controllers\V1\api\Manager\CourseListController;
 
 Route::post('/login', [ManagerController::class, 'login']);
 
 Route::middleware(['auth:sanctum','manager'])->group(function () {
+    Route::apiResource('courses',CourseListController::class);
 });
