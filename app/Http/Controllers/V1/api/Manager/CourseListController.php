df1d2d89d7810523e794f41ff91916e145705f08
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 00:37:56 +0500

    I added blade forms of all Courselist CRUD

--- app/Http/Controllers/V1/api/Manager/CourseListController.php
+++ app/Http/Controllers/V1/api/Manager/CourseListController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CourseListStoreRequest;
 use App\Http\Resources\CourseListResource;
 use App\Interfaces\Services\CourseListServiceInterface;
+use App\Models\CourseList;
 use App\Trait\ResponseTrait;
 use Illuminate\Http\Request;
 
@@ -14,35 +15,50 @@ class CourseListController extends Controller
 {
  use ResponseTrait;
      public function __construct(protected CourseListServiceInterface $courseListServiceInterface){}
+     public function indexBlade()
+{
+    $courses = CourseList::all(); 
+
+    return view('manager.courses.index', compact('courses'));
+}
+
     public function index()
     {
         $courses = $this->courseListServiceInterface->index();
         return $this->success(CourseListResource::collection($courses));
     }
-
+    public function create(){
+        return view('manager.courses.create');
+    }
     public function store(CourseListStoreRequest $request)
     {
         $courseDTO = new CourseListDTO($request->name,$request->price,$request->period);
         $course = $this->courseListServiceInterface->create($courseDTO);
-        return $this->success(new CourseListResource($course));
+        return redirect()->route('manager.courses')->with('success', 'Kurs muvaffaqiyatli yaratildi!');
     }
-
+     public function showBlade(string $id)
+{
+    $course = CourseList::findOrFail($id);
+    return view('manager.courses.show', compact('course'));
+}
     public function show(string $id)
     {
        $course = $this->courseListServiceInterface->show($id);
       return $this->success(new CourseListResource($course));
     }
-
+     public function updateBlade(string $id){
+        $course = CourseList::findOrFail($id);
+        return view('manager.courses.update',compact('course'));
+     }
     public function update(Request $request, string $id)
     {
        $courseDTO = new CourseListDTO($request->name,$request->price,$request->period);
         $course = $this->courseListServiceInterface->update($id,$courseDTO);
-        return $this->success(new CourseListResource($course));
+        return redirect()->route('manager.courses',compact('course'))->with('success', 'Kurs muvaffaqiyatli yangilandi!');
     }
-
     public function destroy(string $id)
     {
           $course = $this->courseListServiceInterface->destroy($id);
-      return $this->success([],204);
+        return redirect()->route('manager.courses')->with('success', 'Kurs muvaffaqiyatli ochirildi!');
     }
 }
