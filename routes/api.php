eb5a3cbbef7536410246001e0a647ccd8affe445
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 00:41:37 +0500

    I removed routes because of web

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-    Route::prefix('admin')->group(function () {
-        require __DIR__.'/admin.php';   
-    });
-
-    Route::prefix('manager')->group(function () {
-        require __DIR__.'/manager.php';    
-    });
