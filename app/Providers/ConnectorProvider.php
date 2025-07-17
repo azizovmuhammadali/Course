6640e6ad4e7e525283e38f495a59aed6e6414ab6
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 17:52:49 +0500

    Connected teacher  interfaces and classes

--- app/Providers/ConnectorProvider.php
+++ app/Providers/ConnectorProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use App\Interfaces\Reposities\CourseListReposityInterface;
+use App\Interfaces\Reposities\TeacherReposityInterface;
 use App\Interfaces\Services\CourseListServiceInterface;
+use App\Interfaces\Services\TeacherServiceInterface;
 use App\Reposities\CourseListReposity;
+use App\Reposities\TeacherReposity;
 use App\Services\CourseListService;
+use App\Services\TeacherService;
 use Illuminate\Support\ServiceProvider;
 
 class ConnectorProvider extends ServiceProvider
@@ -25,5 +29,7 @@ class ConnectorProvider extends ServiceProvider
     {
         $this->app->bind(CourseListServiceInterface::class,CourseListService::class);
         $this->app->bind(CourseListReposityInterface::class,CourseListReposity::class);
+        $this->app->bind(TeacherServiceInterface::class,TeacherService::class);
+        $this->app->bind(TeacherReposityInterface::class,TeacherReposity::class);
     }
 }
