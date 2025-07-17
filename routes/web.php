5f9f8ebc65c01851b66378e4d611db73f958fed1
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 10:16:33 +0500

    I added name to welcome blade

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 Route::prefix('admin')->group(function () {
           require __DIR__.'/admin.php';   
 });
