c248d13c4841672ea7edcadcd0544f7341be3469
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 10:15:46 +0500

    Added middlewares to routes

--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\api\Admin\AdminController;
 
 Route::get('/login', [AdminController::class, 'loginBlade'])->name('admin.login');
+Route::middleware('admin')->group(function(){
+
+});
--- routes/manager.php
+++ routes/manager.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\V1\api\Manager\CourseListController;
 
  Route::get('/login', [ManagerController::class, 'loginBlade'])->name('manager.login');
     Route::post('/login', [ManagerController::class, 'login'])->name('manager.login.submit');
-    Route::get('index',[CourseListController::class,'indexBlade'])->name('manager.courses');
+Route::middleware('admin')->group(function(){
+ Route::get('index',[CourseListController::class,'indexBlade'])->name('manager.courses');
     Route::get('create',[CourseListController::class,'create'])->name('manager.courses.create');
     Route::post('store',[CourseListController::class,'store'])->name('manager.courses.store');
     Route::get('show/{id}',[CourseListController::class,'showBlade'])->name('manager.courses.show');
     Route::get('update/{id}',[CourseListController::class,'updateBlade'])->name('manager.courses.update');
     Route::post('updatecorse/{id}',[CourseListController::class,'update'])->name('manager.courses.updated');
     Route::delete('delete/{id}',[CourseListController::class,'destroy'])->name('manager.courses.delete');
+});
