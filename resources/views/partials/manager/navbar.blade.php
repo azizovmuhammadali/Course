b56af00747e77506052a38c92c99f4a0f3e2bc7c
azizovmuhammadali <alico.777777@gmail.com>
2025-07-17 17:53:46 +0500

    Created main parts of the project

---
+++ resources/views/partials/manager/navbar.blade.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Manager Panel</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="{{route('manager.courses')}}">Kurslar</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{route('manager.teachers')}}">O'qituvchilar</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
