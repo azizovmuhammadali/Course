507de16ccfa29bebaa25eddfcc96de2d2247bc53
azizovmuhammadali <alico.777777@gmail.com>
2025-07-18 19:27:29 +0500

    reemoved logout function

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -15,7 +15,6 @@
             <div class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item"><a class="nav-link" href="{{ route('students') }}">O'quvchilar</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Logout</a></li>
                 </ul>
             </div>
         </div>
