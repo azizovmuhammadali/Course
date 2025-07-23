4ee2efa6b6eaed72ee3abae7229e2af2fb76c213
azizovmuhammadali <alico.777777@gmail.com>
2025-07-23 11:22:59 +0500

    Added payment routes

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\api\Admin\AdminController;
+use App\Http\Controllers\V1\api\Admin\PaymentController;
 use App\Http\Controllers\V1\api\Admin\StudentController;
 
 Route::get('/login', [AdminController::class, 'loginBlade'])->name('admin.login');
@@ -14,4 +15,11 @@ Route::middleware('admin')->group(function(){
   Route::get('students/update/{id}',[StudentController::class,'updated'])->name('students.update');
   Route::put('students/edit/{id}',[StudentController::class,'update'])->name('students.edit');
   Route::delete('students/delete/{id}',[StudentController::class,'destroy'])->name('students.delete');
+ Route::get('payments/index', [PaymentController::class, 'index'])->name('payments.index');
+Route::get('payments/create', [PaymentController::class, 'create'])->name('payments.create');
+Route::post('payments/store', [PaymentController::class, 'store'])->name('payments.store');
+Route::get('payments/show/{id}', [PaymentController::class, 'show'])->name('payments.show');
+Route::get('payments/edit/{id}', [PaymentController::class, 'edit'])->name('payments.edit');
+Route::put('payments/update/{id}', [PaymentController::class, 'update'])->name('payments.update');
+Route::delete('payments/delete/{id}', [PaymentController::class, 'destroy'])->name('payments.delete');
 });
