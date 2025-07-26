f59992f9685acb8080eaef8d6aaaa2f69a24a13f
azizovmuhammadali <alico.777777@gmail.com>
2025-07-27 01:33:19 +0500

    Connected coursestudent interfaces and classes

--- app/Providers/ConnectorProvider.php
+++ app/Providers/ConnectorProvider.php
@@ -3,18 +3,22 @@
 namespace App\Providers;
 
 use App\Interfaces\Reposities\CourseListReposityInterface;
+use App\Interfaces\Reposities\CourseStudentReposityInterface;
 use App\Interfaces\Reposities\PaymentReposityInterface;
 use App\Interfaces\Reposities\StudentReposityInterface;
 use App\Interfaces\Reposities\TeacherReposityInterface;
 use App\Interfaces\Services\CourseListServiceInterface;
+use App\Interfaces\Services\CourseStudentServiceInterface;
 use App\Interfaces\Services\PaymentServiceInterface;
 use App\Interfaces\Services\StudentServiceInterface;
 use App\Interfaces\Services\TeacherServiceInterface;
 use App\Reposities\CourseListReposity;
+use App\Reposities\CourseStudentReposity;
 use App\Reposities\PaymentReposity;
 use App\Reposities\StudentReposity;
 use App\Reposities\TeacherReposity;
 use App\Services\CourseListService;
+use App\Services\CourseStudentService;
 use App\Services\PaymentService;
 use App\Services\StudentService;
 use App\Services\TeacherService;
@@ -43,5 +47,7 @@ class ConnectorProvider extends ServiceProvider
         $this->app->bind(StudentServiceInterface::class,StudentService::class);
         $this->app->bind(PaymentServiceInterface::class,PaymentService::class);
         $this->app->bind(PaymentReposityInterface::class,PaymentReposity::class);
+        $this->app->bind(CourseStudentServiceInterface::class,CourseStudentService::class);
+        $this->app->bind(CourseStudentReposityInterface::class,CourseStudentReposity::class);
     }
 }
